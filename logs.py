5cd8070d49393690751b9134de45b357d2c0aeb5
vaibhavirohilla741 <62430272+vaibhavirohilla741@users.noreply.github.com>
2020-12-12 14:51:38 +0000

    Add files via upload

---
+++ logs.py
@@ -0,0 +1,19 @@
+from azureml.core import Workspace
+from azureml.core.webservice import Webservice
+
+# Requires the config to be downloaded first to the current working directory
+ws = Workspace.from_config()
+
+# Set with the deployment name
+name = "deploy"
+
+# load existing web service
+service = Webservice(name=name, workspace=ws)
+
+# enable application insight
+service.update(enable_app_insights=True)
+
+logs = service.get_logs()
+
+for line in logs.split('\n'):
+    print(line)
